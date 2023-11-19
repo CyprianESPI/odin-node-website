38142b2d449b70531e39a948b61e706654c8088a
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-19 17:43:06 +0100

    Hello express :)

--- index.js
+++ index.js
@@ -3,39 +3,15 @@
 
 
 // https://www.youtube.com/watch?v=tjAeCLLwZtE
-const http = require('http');
-//const hostname = '127.0.0.1';
-const hostname = "172.21.76.61";
-const port = 8080;
+const express = require("express");
+const app = express();
+const port = 3000;
+const hostname = "localhost";
 
-var os = require('os');
-
-var networkInterfaces = os.networkInterfaces();
-
-console.log(networkInterfaces);
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-
-    requested_url = req.url.toString();
-    console.log(requested_url);
-
-
-    var page_name = "404";
-    if (requested_url === "/" || requested_url === "/index")
-        page_name = "index";
-    else if (requested_url === "/about")
-        page_name = "about";
-    else if (requested_url === "/contact-me")
-        page_name = "contact-me";
-
-    var fs = require("fs");
-    const file_path = "./public_pages/" + page_name + ".html";
-    var text = fs.readFileSync(file_path, "utf-8");
-    res.write(text);
-    res.end();
+app.get("/", (req, res) => {
+    res.send("Hello express!");
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
