ddce3f63d5247d1f47b8225d5bee53c684612372
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-19 19:07:44 +0100

    Open up server to remote connections

--- index.js
+++ index.js
@@ -3,15 +3,32 @@
 
 
 // https://www.youtube.com/watch?v=tjAeCLLwZtE
-const express = require("express");
-const app = express();
-const port = 3000;
-const hostname = "localhost";
+const express = require('express');
+const path = require('path');
 
-app.get("/", (req, res) => {
-    res.send("Hello express!");
+const app = express();
+const PORT = 3000;
+
+// Set the path to the directory where your HTML files reside
+const publicDirectoryPath = path.join(__dirname, 'public_pages');
+
+// Serve static files from the public directory
+app.use(express.static(publicDirectoryPath));
+
+// Routes for different HTML pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicDirectoryPath, 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(publicDirectoryPath, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(publicDirectoryPath, 'contact.html'));
+});
+
+// Start the server, listening on all available network interfaces
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
